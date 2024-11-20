ac08e7eeaae197c7a8809ab130bb93fe3c1da71a
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2024-11-20 20:32:23 +0300

    API gateway with ocelot

--- Resources/API2/Program.cs
+++ Resources/API2/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "API 2" + args[0]);
+app.MapGet("/", () => "API 2 - " + args[0]);
 
 app.Run();
